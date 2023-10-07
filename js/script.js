ff3cd5f46ef2e264fbe16f3511500a2489c3c7a9
uni1224 <saekahane99@gmail.com>
2023-10-07 11:14:47 +0900

    signup

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ $(function() {
     $('#login-show').click(function() {
         $('#login-modal').fadeIn();
     })
+    $('.signup-show').click(function() {
+        $('#signup-modal').fadeIn();
+    })
 });
