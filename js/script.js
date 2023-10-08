ed83bdcd46d71a87c49c2740929e2d86ebd8d76e
uni1224 <saekahane99@gmail.com>
2023-10-08 11:17:05 +0900

    acordion

--- js/script.js
+++ js/script.js
@@ -17,4 +17,17 @@ $(function() {
             $(this).find('.text-contents').removeClass('text-active');
         }
         );
+    $('.faq-list-item').click(function() {
+        var $answer = $(this).find('.answer')
+        if ($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.slideUp();
+            $(this).find('span').text('+');
+        } else {
+            $answer.addClass('open');
+            $answer.slideDown();
+            $(this).find('span').text('-');
+        }
+    })
+
 });
